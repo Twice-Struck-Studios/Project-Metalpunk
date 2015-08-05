eb9873ebd5f701e560b4a58a5e25895c3471bf4e
Benjamin <brpylko@gmail.com>
2015-08-05 15:02:02 -0400

    Added license header to source files

--- src/com/twicestruck/metalpunk/server/TCPServer.java
+++ src/com/twicestruck/metalpunk/server/TCPServer.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2015 Benjamin Pylko
+ * Distributed under the GNU GPL v3
+ * For full terms see GPL in this project's root directory
+ */
+
 package com.twicestruck.metalpunk.server;
 
 import com.twicestruck.metalpunk.common.TCPSocket;
